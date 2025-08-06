8af6674d73b18153f97727474714322516d6fbb2
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-08-06 11:40:26 +0300

    Add click handlers to navigation buttons with alert placeholders

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ const messages = ["Learn React *", "Apply for job", "Invest your new income"];
 export default function App() {
   const step = 1;
 
+  function handlePrevious() {
+    alert("Previous");
+  }
+
+  function handleNext() {
+    alert("Next");
+  }
   return (
     <div className="steps">
       <div className="numbers">
@@ -14,11 +21,17 @@ export default function App() {
       <p className="message">Step: {messages[step - 1]}</p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
 
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
